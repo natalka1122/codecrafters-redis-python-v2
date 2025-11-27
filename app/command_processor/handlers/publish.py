fc1e809f9b106329d3336c120071efb0fdbfbc61
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-11-27 15:17:12 +0100

    codecrafters submit [skip ci]

--- app/command_processor/handlers/publish.py
+++ app/command_processor/handlers/publish.py
@@ -4,6 +4,7 @@ from app.connection.connection import Connection
 from app.redis_state import RedisState
 from app.resp.base import RESPType
 from app.resp.error import Error
+from app.resp.integer import Integer
 
 
 async def handle_publish(
@@ -12,4 +13,5 @@ async def handle_publish(
     """Handle PUBLISH command."""
     if len(args) != 2:
         return Error("ERR wrong number of arguments for 'publish' command")
-    return Error("NotImplementedError")
+    result = len(redis_state.pubsub.get_by_pub(args[0]))
+    return Integer(result)
