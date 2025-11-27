da4a88ab83d38af7aaee5f0fa918d96eb3a33193
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-11-27 15:28:47 +0100

    codecrafters submit [skip ci]

--- app/command_processor/handlers/publish.py
+++ app/command_processor/handlers/publish.py
@@ -1,17 +1,26 @@
+import asyncio
 from typing import Any
 
 from app.connection.connection import Connection
 from app.redis_state import RedisState
+from app.resp.array import Array
 from app.resp.base import RESPType
+from app.resp.bulk_string import BulkString
 from app.resp.error import Error
 from app.resp.integer import Integer
 
 
-async def handle_publish(
+async def handle_publish(  # noqa: WPS210
     args: list[str], redis_state: RedisState, connection: Connection
 ) -> RESPType[Any]:
     """Handle PUBLISH command."""
     if len(args) != 2:
         return Error("ERR wrong number of arguments for 'publish' command")
-    result = len(redis_state.pubsub.get_by_pub(args[0]))
+    pub = args[0]
+    message = [BulkString("message"), BulkString(pub), BulkString(args[1])]
+    message_bytes = Array(message).to_bytes
+    subs = redis_state.pubsub.get_by_pub(pub)
+    result = len(subs)
+    for sub in subs:
+        asyncio.create_task(sub.write(message_bytes))  # TODO
     return Integer(result)
