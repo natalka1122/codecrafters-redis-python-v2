e2df69d3a7fac2ad41e24c0b4e809e1972928afc
EXTERNAL Konovalova Natalia (EPAM, ETAS/ECI) <4388005+natalka1122@users.noreply.github.com>
2025-11-27 21:20:43 +0100

    codecrafters submit [skip ci]

--- app/command_processor/handlers/auth.py
+++ app/command_processor/handlers/auth.py
@@ -3,14 +3,21 @@ from typing import Any
 from app.connection.connection import Connection
 from app.redis_state import RedisState
 from app.resp.base import RESPType
-from app.resp.bulk_string import BulkString
 from app.resp.error import Error
+from app.resp.simple_string import SimpleString
+from app.user import Flags, password_to_hash
 
 
 async def handle_auth(
     args: list[str], redis_state: RedisState, connection: Connection
 ) -> RESPType[Any]:
     """Handle AUTH command."""
-    if args:
-        return Error(f"AUTH command should not have arguments. args = {args}")
-    return BulkString("AUTH: NotImplementedError")
+    if len(args) != 2:
+        return Error(f"AUTH command should have two arguments. args = {args}")
+    username = args[0]
+    password_hashed = password_to_hash(args[1])
+    user = redis_state.users.get(username)
+    if user is not None:
+        if Flags.NOPASS in user.flags or password_hashed in user.passwords:
+            return SimpleString("OK")
+    return Error("WRONGPASS invalid username-password pair or user is disabled.")
--- app/user.py
+++ app/user.py
@@ -7,11 +7,15 @@ class Flags(StrEnum):
     NOPASS = "nopass"
 
 
+def password_to_hash(password: str) -> str:
+    return hashlib.sha256(password.encode("utf-8")).hexdigest()
+
+
 class User:
     def __init__(self, flags: Optional[set[str]] = None) -> None:
         self.flags: set[str] = flags if flags else set()
         self.passwords: set[str] = set()
 
     def add_password(self, password: str) -> None:
-        self.passwords.add(hashlib.sha256(password.encode("utf-8")).hexdigest())
+        self.passwords.add(password_to_hash(password))
         self.flags.discard(Flags.NOPASS)
